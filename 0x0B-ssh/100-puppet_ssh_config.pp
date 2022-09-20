ab3d091253c8e5d00c4ecd471e09a1a592a16af0
JuliusDgenius <ibejulius1@gmail.com>
2022-09-20 08:02:34 +0100

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,10 +3,10 @@ file_line {'Turn off passwd auth':
   ensure => 'present',
   path 	 => '/etc/ssh/ssh_config',
   line	 => '	PasswordAuthentication no',
-  match	 => 'PasswordAuthentication yes'
+  match	 => 'PasswordAuthentication yes',
 }
-file_line {'Declare identify file':
+file_line {'Declare identity file':
   ensure => 'present',
   path	 => '/etc/ssh/ssh_config',
-  line	 => '	IdentifyFile ~/.ssh.school',
+  line	 => '	IdentityFile ~/.ssh/school',
 }
