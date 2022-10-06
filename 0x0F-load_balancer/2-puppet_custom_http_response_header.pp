b613eff7331ac7a53284b09215b17a871a19588d
JuliusDgenius <ibejulius1@gmail.com>
2022-10-06 21:09:17 +0100

    puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,13 @@
+#Time to practice configuring your server with Puppet! Just as
+# before, you should install and configure an Nginx server using
+# Puppet instead of Bash.
+#add HTTP response header
+
+
+exec { 'http header':
+  command => 'apt-get update -y;
+  apt-get install nginx -y;
+  sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart;',
+  provider => shell,
+}
