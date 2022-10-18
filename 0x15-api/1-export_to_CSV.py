6c14eb49c06569509cc614a8d8f82c07134cba9a
JuliusDgenius <ibejulius1@gmail.com>
2022-10-18 16:11:49 +0100

    task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Exports to-do list information for a givrn employee ID to CSV format."""
+import csv
 import requests
 import sys
-import csv
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
