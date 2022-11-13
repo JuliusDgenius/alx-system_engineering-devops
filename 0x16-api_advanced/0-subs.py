6ffb09e59490db9f35ba5155cdadefcc31b500c4
JuliusDgenius <ibejulius1@gmail.com>
2022-11-13 03:34:31 +0100

    task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,10 +11,10 @@ BASE_URL = 'https://www.reddit.com'
 
 def number_of_subscribers(subreddit):
     '''Retrieves the number of subscribers in a given subreddit.
-    ...
+    '''
     api_headers = {
         'Accept': 'application/json',
-        'User-Agent': ' '.jpin([
+        'User-Agent': ' '.join([
              'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
              'AppleWebKit/537.36 (KHMTML, like Gecko)',
              'Chrome/97.0.4692.71',
@@ -24,7 +24,7 @@ def number_of_subscribers(subreddit):
     }
     res = requests.get(
          '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
-         header=api_headers,
+         headers=api_headers,
          allow_redirects=False
     )
     if res.status_code == 200:
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,7 +12,7 @@ BASE_URL = 'https://www.reddit.com'
 def top_ten(subreddit):
     '''Retrieves the title of the top ten posts from a given subreddit.
     '''
-     api_headers = {
+    api_headers = {
         'Accept': 'application/json',
         'User-Agent': ' '.join([
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
@@ -25,14 +25,14 @@ def top_ten(subreddit):
     sort = 'top'
     limit = 10
     res = requests.get(
-            '{}/r/{}/.json?sort={}&limit={}'.format(
-                BASE_URL,
-                subreddit,
-                sort,
-                limit
-            ),
-            headers=api_headers,
-            allow_redirects=False
+        '{}/r/{}/.json?sort={}&limit={}'.format(
+            BASE_URL,
+            subreddit,
+            sort,
+            limit
+        ),
+        headers=api_headers,
+        allow_redirects=False
     )
     if res.status_code == 200:
         for post in res.json()['data']['children'][0:10]:
