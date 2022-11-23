1b3d8b404f718f19096ea3ed504dc82c02248bc0
JuliusDgenius <ibejulius1@gmail.com>
2022-11-23 16:24:33 +0100

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixes 'phpp' error to 'php' with a WordPress Website
-exec { '/var/www/html/wp-setting.php':
-  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
-  path	  => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+exec { 'fix-wordpress':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path	  => '/usr/local/bin/:/bin'
 }
