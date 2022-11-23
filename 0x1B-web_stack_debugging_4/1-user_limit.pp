004ca8b00d61e366a5a1272ec8b5ce488bd02cc4
JuliusDgenius <ibejulius1@gmail.com>
2022-11-23 17:10:46 +0100

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,12 @@
 
 # Increase hard file limit for user
 exec { 'increase-hard-file-limit-for-user':
-  command => 'sed -i "juliusdgenius hard/s/5/50000/" /etc/security/limits.conf',
+  command => 'sed -i "holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
 # Increase soft file limit for user.
 exec { 'increase-soft-file-limit-for-user':
-  command => 'sed -i "/juliusdgenius soft/s/4/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
