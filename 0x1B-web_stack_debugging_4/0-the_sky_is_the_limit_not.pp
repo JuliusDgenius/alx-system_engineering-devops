3bb7052200c402dc2f0a0b7ec76cea14f587cdbd
JuliusDgenius <ibejulius1@gmail.com>
2023-08-05 16:34:08 +0100

    add change

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 
 # Increase The ULIMIT of the default file
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096" /etc/default/nginx',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
