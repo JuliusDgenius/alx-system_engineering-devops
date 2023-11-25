9449d62dbe52a0676d55519807c34455420934b7
JuliusDgenius <ibejulius1@gmail.com>
2023-11-25 05:17:38 +0100

    Task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creating a file with puppet in /tmp directory
+
+file { '0-create_a_file':
+  path     => '/tmp/school',
+  owner    => 'www-data',
+  group    => 'www-data',
+  mode     => '0744',
+  content  => 'I love Puppet',
+}
