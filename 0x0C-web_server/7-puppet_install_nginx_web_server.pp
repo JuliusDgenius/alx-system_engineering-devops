7938e2d6b0c904923b3b93c9589bc2c642a3d2d3
JuliusDgenius <ibejulius1@gmail.com>
2023-11-28 15:32:24 +0100

    Task 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,22 @@
+# Practice configuring a web server with puppet.
+# Install Nginx with puppet
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'install':
+    ensure => 'present',
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'listen 80 default_server;',
+    line   => 'rewrite ^/redirect_me https://juliusdgenius-alx.tech permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
