bff6efaf1227e55b5c905694a9d42ff598e6af31
JuliusDgenius <ibejulius1@gmail.com>
2024-07-18 10:46:23 +0100

    Change Werkzeug version to 2.1.1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
+# Ensure pip3 is installed
+package { 'python3-pip':
+    ensure => installed,   
+}
+
 # Install flask from pip3 using puppet
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require => Package['python3-pip'],
+}
+
+package { 'Werkzeug':
+    ensure => '2.1.1',
+    provider => 'pip3',
+    
 }
