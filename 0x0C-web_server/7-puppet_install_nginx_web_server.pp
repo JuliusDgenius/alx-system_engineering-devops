c9038f1f1eaa4ec9400eb58ef723cef54626e292
JuliusDgenius <ibejulius1@gmail.com>
2024-08-09 01:43:26 +0100

    correction

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,7 +9,7 @@ file_line { 'install':
     ensure => 'present',
     path   => '/etc/nginx/sites-available/default',
     after  => 'listen 80 default_server;',
-    line   => 'rewrite ^/redirect_me https://juliusdgenius-alx.tech permanent;',
+    line   => 'rewrite ^/redirect_me https://juliusdgenius.tech permanent;',
 }
 
 file { '/var/www/html/index.html':
