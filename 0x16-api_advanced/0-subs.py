1a89bfa99b98240689e9fc9d408829b734ba680e
JuliusDgenius <ibejulius1@gmail.com>
2024-08-14 05:34:54 +0100

    correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,15 +11,16 @@ def number_of_subscribers(subreddit):
     Function that returns all the subscribers of a given subreddit,
     not active users
     """
-
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers=user_agent)
-    data = response.json()
+    results = response.json()
+
     try:
-        return data.get('data').get('subscribers')
+        return results.get('data').get('subscribers')
+
     except Exception:
         return 0
